f56cb8ba01e178b20d683ca8a2d64a22af0c88b8
Devinne Leigh <devinneleigh@gmail.com>
2025-04-29 15:20:29 -0600

    created chapter 11

---
+++ Chapter11/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
